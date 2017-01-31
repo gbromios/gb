a9d9517ab56ea621a878c9d93b30572b32444988
steven <steven@themoonischill.net>
2017-01-31 09:36:17 -0500

    just a variable name change.

--- http/server.py
+++ http/server.py
@@ -4,8 +4,8 @@ from handler import Handler
 
 class Server(SocketServer.ForkingMixIn, BaseHTTPServer.HTTPServer):
 	""" basic threaded http server """
-	def __init__(self, server_address, handler):
-		BaseHTTPServer.HTTPServer.__init__(self, server_address, handler)
+	def __init__(self, listen, handler):
+		BaseHTTPServer.HTTPServer.__init__(self, listen, handler)
 		if not issubclass(handler, Handler):
 			raise TypeError('gb.http.Server requires gb.http.Handler for its handler.')
 
